926a1b2d916a534b51fccab2f53c3fb221387881
triniti <787558@ukr.net>
2021-02-03 15:36:59 +0200

    Homework28 Android

--- src/test/java/mobile/NativeAppTest.java
+++ src/test/java/mobile/NativeAppTest.java
@@ -15,6 +15,7 @@ import static org.testng.Assert.assertEquals;
 
 public class NativeAppTest {
     AppiumDriver appiumDriver;
+    WebElement one, two, three, four, five, six, nine, eight, point, div, plus, sub, del, mult, eq, results;
 
     @BeforeMethod
     public void setup() throws MalformedURLException {
@@ -33,30 +34,37 @@ public class NativeAppTest {
         appiumDriver = new AppiumDriver(serverAddress, capabilities);
     }
 
+    private void findElements() {
+        one = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_1')]"));
+        two = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_2')]"));
+        three = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_3')]"));
+        four = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_4')]"));
+        five = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_5')]"));
+        six = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_6')]"));
+        eight = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_8')]"));
+        nine = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_9')]"));
+        plus = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'op_add')]"));
+        div = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'op_div')]"));
+        point = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'dec_point')]"));
+        eq = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'eq')]"));
+        results = appiumDriver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, 'result')]"));
+        mult = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'op_mul')]"));
+        del = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'del')]"));
+    }
+
     @Test
     public void testCalc() throws Exception {
-        WebElement two = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_2')]"));
+        findElements();
         two.click();
-        WebElement plus = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'op_add')]"));
         plus.click();
-        WebElement three = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_3')]"));
         three.click();
-        WebElement eq = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'eq')]"));
         eq.click();
-        WebElement results = appiumDriver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, 'result')]"));
         assertEquals("5", results.getText(), "Check the calculator result is 5");
     }
 
     @Test
     public void testCalcMult() throws Exception {
-        WebElement nine = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_9')]"));
-        WebElement mult = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'op_mul')]"));
-        WebElement three = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_3')]"));
-        WebElement four = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_4')]"));
-        WebElement div = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'op_div')]"));
-        WebElement del = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'del')]"));
-        WebElement eq = appiumDriver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'eq')]"));
-        WebElement results = appiumDriver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, 'result')]"));
+        findElements();
         nine.click();
         mult.click();
         nine.click();
@@ -67,4 +75,38 @@ public class NativeAppTest {
         eq.click();
         assertEquals("27", results.getText(), "Check the calculator result is 27");
     }
+
+    @Test
+    public void testCalcFloat() throws Exception {
+        findElements();
+        eight.click();
+        point.click();
+        six.click();
+        sub.click();
+        six.click();
+        point.click();
+        two.click();
+        eq.click();
+        assertEquals("2.4", results.getText(), "Check the calculator result is 2.4");
+    }
+
+    @Test
+    public void testCalcFloat2() throws Exception {
+        findElements();
+        five.click();
+        point.click();
+        one.click();
+        one.click();
+        mult.click();
+        nine.click();
+        point.click();
+        five.click();
+        div.click();
+        three.click();
+        del.click();
+        four.click();
+        eq.click();
+        assertEquals("12.13625", results.getText(), "Check the calculator result is 12.13625");
+    }
+
 }
